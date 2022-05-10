cd17415896feb09e29a23723e90be5effb78fe99
imjyotiraditya <jyotiraditya@aospa.co>
2022-05-10 21:05:50 +0600

    build.sh: Allow passing HOME_DIR and TC_DIR.
    Signed-off-by: Jyotiraditya Panda <jyotiraditya@aospa.co>
    Signed-off-by: BladeRunner-A2C <prisoner56898@protonmail.com>

--- build.sh
+++ build.sh
@@ -3,47 +3,98 @@
 # Compile script for QuicksilveR kernel
 # Copyright (C) 2020-2021 Adithya R.
 
+# Setup getopt.
+long_opts="regen,clean,homedir:,tcdir:"
+getopt_cmd=$(getopt -o rch:t: --long "$long_opts" \
+            -n $(basename $0) -- "$@") || \
+            { echo -e "\nError: Getopt failed. Extra args\n"; exit 1;}
+
+eval set -- "$getopt_cmd"
+
+while true; do
+    case "$1" in
+        -r|--regen|r|regen) FLAG_REGEN_DEFCONFIG=y;;
+        -c|--clean|c|clean) FLAG_CLEAN_BUILD=y;;
+        -h|--homedir|h|homedir) HOME_DIR="$2"; shift;;
+        -t|--tcdir|t|tcdir) TC_DIR="$2"; shift;;
+        -o|--outdir|o|outdir) OUT_DIR="$2"; shift;;
+        --) shift; break;;
+    esac
+    shift
+done
+
+# Setup HOME dir
+if [ $HOME_DIR ]; then
+    HOME_DIR=$HOME_DIR
+else
+    HOME_DIR=$HOME
+fi
+echo -e "HOME directory is at $HOME_DIR\n"
+
+# Setup Toolchain dir
+if [ $TC_DIR ]; then
+    TC_DIR="$HOME_DIR/$TC_DIR"
+else
+    TC_DIR="$HOME_DIR/tc"
+fi
+echo -e "Toolchain directory is at $TC_DIR\n"
+
+# Setup OUT dir
+if [ $OUT_DIR ]; then
+    OUT_DIR=$OUT_DIR
+else
+    OUT_DIR=out
+fi
+echo -e "Out directory is at $OUT_DIR\n"
+
+export KBUILD_BUILD_USER=007
+export KBUILD_BUILD_HOST=skyfall
+
 SECONDS=0 # builtin bash timer
 ZIPNAME="QuicksilveR-miatoll-$(date '+%Y%m%d-%H%M').zip"
 if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
    head=$(git rev-parse --verify HEAD 2>/dev/null); then
         ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8).zip"
 fi
-TC_DIR="$HOME/tc/clang-r445002"
-GCC_64_DIR="$HOME/tc/aarch64-linux-android-4.9"
-GCC_32_DIR="$HOME/tc/arm-linux-androideabi-4.9"
+CLANG_DIR="$TC_DIR/clang-r445002"
+GCC_64_DIR="$TC_DIR/aarch64-linux-android-4.9"
+GCC_32_DIR="$TC_DIR/arm-linux-androideabi-4.9"
 DEFCONFIG="vendor/miatoll-perf_defconfig"
 
-export PATH="$TC_DIR/bin:$PATH"
+export PATH="$CLANG_DIR/bin:$PATH"
 
-if [[ $1 = "-c" || $1 = "--clean" ]]; then
-	rm -rf out
+# Prep for a clean build, if requested so
+if [ "$FLAG_CLEAN_BUILD" = 'y' ]; then
+	echo -e "\nCleaning output folder..."
+	rm -rf $OUT_DIR
 fi
 
-if [[ $1 = "-r" || $1 = "--regen" ]]; then
-	make O=out ARCH=arm64 $DEFCONFIG savedefconfig
-	cp out/defconfig arch/arm64/configs/$DEFCONFIG
+# Regenerate defconfig, if requested so
+if [ "$FLAG_REGEN_DEFCONFIG" = 'y' ]; then
+	make O=$OUT_DIR ARCH=arm64 $DEFCONFIG savedefconfig
+	cp $OUT_DIR/defconfig arch/arm64/configs/$DEFCONFIG
+	echo -e "\nSuccessfully regenerated defconfig at $DEFCONFIG"
 	exit 1
 fi
 
-mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
+mkdir -p $OUT_DIR
+make O=$OUT_DIR ARCH=arm64 $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
-make -j"$(nproc --all)" O=out ARCH=arm64 HOSTCC=clang HOSTLD=ld.lld CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image dtbo.img
+make -j"$(nproc --all)" O=$OUT_DIR ARCH=arm64 HOSTCC=clang HOSTLD=ld.lld CC=clang LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=$GCC_64_DIR/bin/aarch64-linux-android- CROSS_COMPILE_ARM32=$GCC_32_DIR/bin/arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image dtbo.img
 
-if [ -f "out/arch/arm64/boot/Image" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+if [ -f "$OUT_DIR/arch/arm64/boot/Image" ] && [ -f "$OUT_DIR/arch/arm64/boot/dtbo.img" ]; then
 	echo -e "\nKernel compiled succesfully! Zipping up...\n"
 	if ! git clone -q https://github.com/BladeRunner-A2C/AnyKernel3 -b miatoll; then
 		echo -e "\nCloning AnyKernel3 repo failed! Aborting..."
 		exit 1
 	fi
-	cp out/arch/arm64/boot/Image AnyKernel3
-	cp out/arch/arm64/boot/dtbo.img AnyKernel3
-	cp out/arch/arm64/boot/dts/qcom/cust-atoll-ab.dtb AnyKernel3/dtb
+	cp $OUT_DIR/arch/arm64/boot/Image AnyKernel3
+	cp $OUT_DIR/arch/arm64/boot/dtbo.img AnyKernel3
+	cp $OUT_DIR/arch/arm64/boot/dts/qcom/cust-atoll-ab.dtb AnyKernel3/dtb
 	rm -f ./*zip
 	cd AnyKernel3 || exit
-	rm -rf out/arch/arm64/boot
+	rm -rf $OUT_DIR/arch/arm64/boot
 	zip -r9 "../$ZIPNAME" ./* -x '*.git*' README.md ./*placeholder
 	cd ..
 	rm -rf AnyKernel3
